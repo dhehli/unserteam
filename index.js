2afef8832455bafbbaad7cfee24666c1b92ccb7a
dhehli <dh@netlive.ch>
2017-08-22 22:52:16 +0200

    add config

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const config = require('config');
 
 app.use('/', express.static(__dirname + '/public'))
 
@@ -7,6 +8,6 @@ app.get('/', function (req, res) {
   res.render(__dirname + '/views/index.pug')
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(config.port, function () {
+  console.log(`Example app listening on port ${config.port}`)
 })
