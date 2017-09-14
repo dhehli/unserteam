96d7622b16fefa327cb643cffe2195cd6a946482
dhehli <dh@netlive.ch>
2017-09-14 21:29:44 +0200

    add member with filterable list

--- public/js/member.js
+++ public/js/member.js
@@ -1,69 +1,91 @@
-$(document).on("pagecontainershow", function () {
+$(document).on("pagebeforeshow", function () {
   const pageId = $('body').pagecontainer('getActivePage').prop('id');
 
   //Load Team and Members
-  if(pageId === "member"){
-    ajaxHandler('get', '/api/v1/teams', {}, 'session' , function(msg){
-      // TODO: Handler Errors and Warning
-      if(Array.isArray(msg)){
-        const fieldsetList = [];
+  if(pageId === "team-detail"){
+    const teamId = window.currentId;
 
-        msg.forEach(function(user, index){
-          const { TeamId, Name } = user;
+    function loadAddedMembers(){
+      //Load Memberlist of currentTeam
+      ajaxHandler('get', `/api/v1/teams/${teamId}`, {}, 'session' , function(msg){
+        // TODO: Handler Errors and Warning
+        if(Array.isArray(msg)){
+          const { Members } = msg[0];
 
-          fieldsetList.push(`
-            <fieldset data-role="collapsible" data-id=${TeamId}>
-              <legend>${Name}</legend>
-            </fieldset>
-          `);
+          if(Array.isArray(Members)){
+            const userIdArr = [];
 
-          $("#member-add-form").html(fieldsetList).trigger('create');
+            Members.forEach(function(member){
+              const { UserId, MemberId } = member;
+              userIdArr.push({UserId, MemberId})
+            })
 
-          ajaxHandler('get', '/api/v1/users', {}, 'session' , function(msg){
-            // TODO: Handler Errors and Warning
-            if(Array.isArray(msg)){
-              const $divGroup = $('<div data-role="controlgroup">');
-              const $formElementList = [];
+            const $liList = [];
 
-              msg.forEach(function(user){
-                const { UserId, LastName, FirstName } = user;
+            userIdArr.forEach(function(user){
+              const { UserId, MemberId } = user;
+              ajaxHandler('get', `/api/v1/users/${UserId}`, {}, 'session' , function(msg){
+                  const { UserId, Active, LastName, FirstName, Email } = msg[0];
 
-                $formElementList.push(`
-                  <input
-                    class="cb-member-add" type="checkbox"
-                    name="checkbox-${TeamId}-${UserId}"
-                    id="checkbox-${TeamId}-${UserId}"
-                    data-team-id=${TeamId}
-                    data-user-id=${UserId}
-                  >
-                  <label for="checkbox-${TeamId}-${UserId}">${FirstName} ${LastName}</label>
-                `);
+                  $liList.push(`
+                    <li data-icon="delete"><a href="#" class="remove-member-link" data-id="${MemberId}">${FirstName} ${LastName}</a></li>
+                  `);
+
+                  $(".member-list-added").html($liList).listview('refresh').trigger("create");
+
+                  $(".remove-member-link").click(function(e){
+                    e.preventDefault();
+                    const memberId = $(this).attr("data-id");
+                    ajaxHandler('delete', `/api/v1/members/${memberId}`, {}, 'session' , function(msg){
+                      console.log(msg)
+                      $(".member-list-added").listview('refresh').trigger("create");
+                      loadAddedMembers();
+                    })
+                  })
               })
-              $divGroup.html($formElementList);
+            })
+          }
+        }
+      })
+    }
+    loadAddedMembers();
 
-              $("#member-add-form fieldset[data-id='" + TeamId  + "'] .ui-collapsible-content").html($divGroup).trigger('create');
+    function loadAllMembers(){
+      //Load User List
+      ajaxHandler('get', '/api/v1/users', {}, 'session' , function(msg){
+        // TODO: Handler Errors and Warning
+        if(Array.isArray(msg)){
+          const $liList = [];
 
-              //Add Change Eventhandler
-              $(".cb-member-add").change(function(){
-                const { teamId, userId } = $(this).prop("dataset")
-                if($(this).is(":checked")){
-                    const data = {
-                      TeamId: teamId,
-                      UserId: userId
-                    }
-                    ajaxHandler('post', '/api/v1/members', {}, 'session' , function(msg){
+          msg.forEach(function(user){
+            const { UserId, Active, LastName, FirstName, Email } = user;
 
-                    }
-                }else{
-                    console.log("unchecked")
-                }
+            $liList.push(`
+              <li data-icon="plus"><a href="#" class="add-member-link" data-id="${UserId}">${FirstName} ${LastName}</a></li>
+            `);
+          })
 
-                console.log(teamId, userId)
-              })
-            }
+          $(".member-list-toadd").html($liList).listview('refresh').trigger("create");
+        }
+
+        $(".add-member-link").click(function(e){
+          e.preventDefault();
+          const teamId = window.currentId;
+          const userId = $(this).attr("data-id");
+
+          const data = {
+            UserId: userId,
+            TeamId: teamId
+          }
+
+          ajaxHandler('post', '/api/v1/members', data, 'session' , function(msg){
+            $(".member-list-added").listview('refresh').trigger("create");
+            loadAddedMembers();
           })
         })
-      }
-    })
+      })
+    }
+    loadAllMembers();
+
   }
 })
