3f38e46aeb03f4f8976a036004d84e721ea47a2e
dhehli <dh@netlive.ch>
2017-09-15 17:33:48 +0200

    add training edit anddetail

--- public/js/training.js
+++ public/js/training.js
@@ -11,25 +11,31 @@ $(document).on("pagecontainershow", function () {
         msg.forEach(function(training){
           const { TrainingId, TeamId, Title, Date, Time } = training;
 
-          $trList.push(`
-            <tr>
-              <td>${TrainingId}</td>
-              <td>${Title}</td>
-              <td>${Date}</td>
-              <td>${Time}</td>
-              <td><a href="#training-detail" data-id="${TrainingId}" class="training-link-detail ui-icon-home">Detail</a></td>
-            </tr>
-          `);
+          ajaxHandler('get', `/api/v1/teams/${TeamId}`, {}, 'session' , function(msg){
+            // TODO: Handler Errors and Warning
+            const { Name } = msg[0];
+
+            $trList.push(`
+              <tr>
+                <td>${TrainingId}</td>
+                <td>${Title}</td>
+                <td>${Date}</td>
+                <td>${Time}</td>
+                <td>${Name}</td>
+                <td><a href="#training-detail" data-id="${TrainingId}" class="training-link-detail ui-icon-home">Detail</a></td>
+              </tr>
+            `);
+
+            $(".tbody-training").html($trList);
+
+            //Add Eventhandler for Userdetail
+            $(".training-link-detail").click(function(e){
+              const currentId = $(this).attr("data-id");
+              window.currentId = currentId;
+            })
+          })
         })
-
-        $(".tbody-training").html($trList);
       }
-
-      //Add Eventhandler for Userdetail
-      $(".training-link-detail").click(function(e){
-        const currentId = $(this).attr("data-id");
-        window.currentId = currentId;
-      })
     })
   }
 
@@ -40,28 +46,38 @@ $(document).on("pagecontainershow", function () {
     if(currentTrainingId){
       ajaxHandler('get', `/api/v1/trainings/${currentTrainingId}`, {}, 'session' , function(msg){
         if(Array.isArray(msg)){
-          const $trList = [];
+          const $liList = [];
 
           msg.forEach(function(training){
-            const { TrainingId, TeamId, Title, Date, Time, Description } = training;
+            const {
+              TrainingId, TeamId, Title, Date, Time, Duration, MinParticipants, Description
+            } = training;
 
-            $trList.push(`
-              <tr>
-                <td>${TeamId}</td>
-                <td>${Name}</td>
-                <td>${Website || ''}</td>
-              </tr>
-            `);
+            ajaxHandler('get', `/api/v1/teams/${TeamId}`, {}, 'session' , function(msg){
+              // TODO: Handler Errors and Warning
+              const { Name } = msg[0];
+
+              $liList.push(`
+                <li><label>TrainingId</label> ${TrainingId}</li>
+                <li><label>Title</label> ${Title}</li>
+                <li><label>Team</label> ${Name}</li>
+                <li><label>Date</label> ${Date}</li>
+                <li><label>Time</label> ${Time}</li>
+                <li><label>Duration</label> ${Duration || ''}</li>
+                <li><label>MinParticipants</label> ${MinParticipants || ''}</li>
+                <li><label>Description</label> ${Description || ''}</li>
+              `);
+
+              $(".listview-training-detail").html($liList).listview('refresh').trigger("create");
+            })
           })
-
-          $(".tbody-team-detail").html($trList);
         }
       })
     }
   }
 
   //Load Training Select Field
-  if(pageId === "training-add"){
+  if(pageId === "training-add" || pageId === "training-edit"){
     ajaxHandler('get', '/api/v1/teams', {}, 'session' , function(msg){
       // TODO: Handler Errors and Warning
       if(Array.isArray(msg)){
@@ -76,17 +92,25 @@ $(document).on("pagecontainershow", function () {
         })
 
         $("#team-training-add").html($optionList);
+        $("#team-training-edit").html($optionList);
+
       }
     })
   }
 
   //Read Entry for Teamedit
-  if(pageId === "team-edit") {
-    const currentTeamId = window.currentId;
-    ajaxHandler('get', `/api/v1/teams/${currentTeamId}`, {}, 'session' , function(msg){
-      const { Name, Website } = msg[0];
-      $("#name-team-edit").val(Name);
-      $("#website-team-edit").val(Website);
+  if(pageId === "training-edit") {
+    const currentTrainingId = window.currentId;
+    ajaxHandler('get', `/api/v1/trainings/${currentTrainingId}`, {}, 'session' , function(msg){
+      const { TeamId, Title, Date, Time, Duration, MinParticipants, Description } = msg[0];
+
+      const teamId = $("#team-training-edit").val(TeamId).change();
+      const title = $("#title-training-edit").val(Title);
+      const date = $("#date-training-edit").val(Date);
+      const time = $("#time-training-edit").val(Time);
+      const duration = $("#duration-training-edit").val(Duration);
+      const participants = $("#participants-training-edit").val(MinParticipants);
+      const description = $("#description-training-edit").val(Description);
     })
   }
 })
@@ -129,40 +153,51 @@ $(function(){
     })
   })
 
-  //Submit Team
-  $("#training-add-form").submit(function(e){
+  //Edit Training
+  $("#training-edit-form").submit(function(e){
     e.preventDefault();
+    const currentTrainingId = window.currentId;
 
     const $alert = $(this).find(".alert");
 
-    const ownerId = $.session.get('loggedInUserId');
-    const name = $("#name-team-add").val();
-    const website = $("#website-team-add").val();
+    const teamId = $("#team-training-edit").val();
+    const title = $("#title-training-edit").val();
+    const date = $("#date-training-edit").val();
+    const time = $("#time-training-edit").val();
+    const duration = $("#duration-training-edit").val();
+    const participants = $("#participants-training-edit").val();
+    const description = $("#description-training-edit").val();
 
     const data = {
-      OwnerId: ownerId,
-      Name: name,
-      Website: website
+      TeamId: teamId,
+      Title: title,
+      Date: date,
+      Time: time,
+      Duration: duration,
+      MinParticipants: participants,
+      Description: description
     }
 
-    ajaxHandler('post', '/api/v1/teams', data, 'session' , function(msg){
+    ajaxHandler('put', `/api/v1/trainings/${currentTrainingId}`, data, 'session' , function(msg){
+      console.log(msg)
       if(msg.type == "Warning" || msg.type == "Error"){
         $alert.addClass("alert-danger");
         $alert.text(msg.message);
       }else{
         $alert.addClass("alert-success");
-        $alert.html("Team created, go back to the <a href='#team'>List</a>");
+        $alert.html("Training edited, go back to the <a href='#training'>List</a>");
       }
     })
   })
 
   //Add Eventhandler for Teamdelete
-  $("#team-link-delete").click(function(e){
-    const currentTeamId = window.currentId;
+  $("#training-link-delete").click(function(e){
     e.preventDefault();
+    const currentTrainingId = window.currentId;
+    console.log(currentTrainingId);
 
-    ajaxHandler('delete', `/api/v1/teams/${currentTeamId}`, {}, 'session' , function(msg){
-      $.mobile.pageContainer.pagecontainer("change", "#team");
+    ajaxHandler('delete', `/api/v1/trainings/${currentTrainingId}`, {}, 'session' , function(msg){
+      $.mobile.pageContainer.pagecontainer("change", "#training");
     })
   })
 })
