95897366abb0fd21fa22a17f03a840adc78359d7
dhehli <dh@netlive.ch>
2017-09-17 15:23:47 +0200

    add basic tests

--- test/js/test.js
+++ test/js/test.js
@@ -1,3 +1,19 @@
-QUnit.test( "hello test", function( assert ) {
-  assert.ok( 1 == "1", "Passed!" );
+QUnit.test( "Basic Auth", function( assert ) {
+  assert.ok(basicAuth("dh@netlive.ch","12345678") === "Basic ZGhAbmV0bGl2ZS5jaDoxMjM0NTY3OA==", "Auth ok");
+});
+
+QUnit.test( "Alert Handler Success", function( assert ) {
+  const $alert = $("<div class='alert'>");
+  const $alertElement = alertHandler($alert, "success", "success text");
+
+  assert.ok($alertElement.hasClass("alert-success"), "Has correct Class");
+  assert.ok($alertElement.html() === "success text", "Has correct Text");
+});
+
+QUnit.test( "Alert Handler Danger", function( assert ) {
+  const $alert = $("<div class='alert'>");
+  const $alertElement = alertHandler($alert, "danger", "warning text");
+
+  assert.ok($alertElement.hasClass("alert-danger"), "Has correct Class");
+  assert.ok($alertElement.html() === "warning text", "Has correct Text");
 });
